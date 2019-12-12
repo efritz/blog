4355b693ed813d388d82bf546ea2c6f292402edd
Eric Fritz <eric@eric-fritz.com>
2019-12-12 00:07:34 -0600

    Inline part of deploy script into action.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,15 +1,5 @@
 #!/bin/sh -ex
 
-# Install build requirements
-apt-get update && apt-get install ca-certificates git python python-setuptools
-
-# Download and install s3cmd
-git clone https://github.com/s3tools/s3cmd.git /tmp/s3cmd
-cd /tmp/s3cmd/
-python setup.py install
-cd -
-
-# Configure s3cmd
 cat << EOF > .s3cfg
 [default]
 access_key = ${ACCESS_KEY}
@@ -18,5 +8,4 @@ host_base = sfo2.digitaloceanspaces.com
 host_bucket = %(bucket)s.sfo2.digitaloceanspaces.com
 EOF
 
-# Publish public directory to Digital Ocean bucket as public files
 s3cmd --config .s3cfg put public s3://laniakea/blog/ --acl-public --recursive
