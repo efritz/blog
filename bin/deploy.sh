6d30b604e6a9d08f4272aa3493d6b0ce29ad9366
Eric Fritz <eric@eric-fritz.com>
2019-12-12 00:16:33 -0600

    Replace github actions with circleci config.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -ex
 
+trap "{ rm -f .s3cfg; }" EXIT
+
 cat << EOF > .s3cfg
 [default]
 access_key = ${ACCESS_KEY}
@@ -8,4 +10,14 @@ host_base = sfo2.digitaloceanspaces.com
 host_bucket = %(bucket)s.sfo2.digitaloceanspaces.com
 EOF
 
+# Replace assets
+s3cmd --config .s3cfg del s3://laniakea/blog/public
 s3cmd --config .s3cfg put public s3://laniakea/blog/ --acl-public --recursive
+
+# Set proper content-types for css objects
+s3cmd --config .s3cfg \
+    --recursive modify \
+    --add-header='content-type':'text/css' \
+    --exclude '' \
+    --include '.css' \
+    s3://laniakea/blog/
