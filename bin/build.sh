12fc5e6eb89cc3159da9820091e557dafce9b764
Eric Fritz <eric@eric-fritz.com>
2024-01-21 16:54:55 -0800

    WIP.

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-hugo --gc --minify
+base_args=
+if [ -n "$IS_PULL_REQUEST" ]; then
+    base_args="--baseURL ${RENDER_EXTERNAL_URL}"
+fi
+
+# shellcheck disable=SC2086
+hugo --gc --minify $base_args
