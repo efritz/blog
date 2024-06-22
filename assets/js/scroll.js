360670249867ed527b5abfa91e9701e9a375a980
Eric Fritz <eric@eric-fritz.com>
2024-06-22 18:56:23 -0500

    Add read progress bar (#45)

---
+++ assets/js/scroll.js
@@ -0,0 +1,6 @@
+window.addEventListener('scroll', function() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById('read-progress-bar').style.width = scrolled + '%';
+});
