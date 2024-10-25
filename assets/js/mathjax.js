8eaea3d7dd629324917837330cff09bd8badd73e
Eric Fritz <eric@eric-fritz.com>
2024-10-25 17:13:06 -0500

    Update mathjax config and set default renderer.

--- assets/js/mathjax.js
+++ assets/js/mathjax.js
@@ -2,6 +2,12 @@ MathJax.Hub.Config({
   tex2jax: {
     inlineMath: [['$', '$'], ['\\(', '\\)']],
     processEscapes: true,
-    loader: {load: ["input/tex", "output/chtml"]},
+  },
+  jax: ['input/TeX', 'output/SVG'],
+  extensions: ['tex2jax.js'],
+  SVG: {
+    fontCache: 'global'
   }
 });
+
+MathJax.Hub.setRenderer('SVG');
