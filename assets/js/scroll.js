6578915e062107a26d235638c156dd2cbb21ad5d
Eric Fritz <eric@eric-fritz.com>
2024-10-26 21:58:15 -0500

    WIP.

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -4,19 +4,3 @@ window.addEventListener('scroll', function() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById('read-progress-bar').style.width = scrolled + '%';
 });
-
-window.addEventListener('scroll', function() {
-    $('.article-content h2, .article-content h3, .article-content h4, .article-content h5').each(function(_, header) {
-        const headerTop = header.getBoundingClientRect().top;
-
-        if (0 <= headerTop && headerTop <= window.innerHeight / 2) {
-            $('#toc a').each(function(_, tocLink) {
-                if (tocLink.href.endsWith('#' + header.id)) {
-                    $(tocLink).addClass('current');
-                } else {
-                    $(tocLink).removeClass('current');
-                }
-            });
-        }
-    });
-});
--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,7 +1,26 @@
 
 $(document).ready(function() {
+    // Add 'top' id to the article title
+    $('.articles h2:first').attr('id', 'top');
+
     $('#toc').toc({
-        content: '.article-content',
+        content: '.articles',  // Changed to include the title
         headings: 'h2, h3, h4, h5'
+    });
+});
+
+window.addEventListener('scroll', function() {
+    $('.article-content h2, .article-content h3, .article-content h4, .article-content h5').each(function(_, header) {
+        const headerTop = header.getBoundingClientRect().top;
+
+        if (0 <= headerTop && headerTop <= window.innerHeight / 2) {
+            $('#toc a').each(function(_, tocLink) {
+                if (tocLink.href.endsWith('#' + header.id)) {
+                    $(tocLink).addClass('current');
+                } else {
+                    $(tocLink).removeClass('current');
+                }
+            });
+        }
     });
 });
