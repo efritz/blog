b18f7b4279f97d3165ba2345eb5ac8fb7eb32455
Eric Fritz <eric@eric-fritz.com>
2024-10-30 22:41:30 -0500

    Cleanup.

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,57 +1,25 @@
-const headerHeight = 78;
-const heroBuffer = 20;
+const minFromTop = 10;
+const bufferFromNav = 170;
+const bufferFromHero = 20;
 const tocHeaderSelectors = 'h2, h3, h4, h5, h6';
 
 $(document).ready(function() {
-    const toc = $('#toc');
-    const heroContainer = $('.hero-container');
-
-    toc.toc({
+    $('#toc').toc({
         content: '.articles, .resume',
         headings: tocHeaderSelectors,
     });
 
-    // Always use fixed positioning
-    toc.css('position', 'fixed');
-
     window.addEventListener('scroll', () => {
         updateLink(selectVisibleHeader());
-
-        const hasHero = heroContainer.length > 0;
-        const scrollTop = $(window).scrollTop();
-        const heroHeight = hasHero && heroContainer.offset().top + heroContainer.outerHeight();
-
-        if (hasHero) {
-            // With hero image: smoothly transition between positions
-            if (scrollTop + headerHeight < heroHeight) {
-                const newTop = heroHeight - scrollTop + heroBuffer;
-                toc.css('top', `${newTop}px`);
-            } else {
-                toc.css('top', `${headerHeight}px`);
-            }
-        } else {
-            // Without hero image: adjust based on scroll position
-            if (scrollTop < 92) { // 170 - 78 = 92 (difference between desired initial position and header height)
-                toc.css('top', '170px');
-            } else {
-                toc.css('top', `${headerHeight}px`);
-            }
-        }
+        updateTocPosition();
     });
 
-    // Set initial position
-    if (heroContainer.length) {
-        const initialTop = heroContainer.offset().top + heroContainer.outerHeight() + heroBuffer;
-        toc.css('top', `${initialTop}px`);
-    } else {
-        toc.css('top', '170px');
-    }
+    window.addEventListener('hashchange', () => {
+        updateLink(fromHash());
+    });
 
-    // Highlight link on header copy button press
-    window.addEventListener('hashchange', () => updateLink(fromHash()));
-
-    // Set initial state
     updateLink(fromHash());
+    updateTocPosition();
 });
 
 function selectVisibleHeader() {
@@ -80,4 +48,28 @@ function updateLink(id) {
 
     $('#toc a').removeClass('current');
     $(`#toc a[href="#${id}"]`).addClass('current');
+}
+
+function updateTocPosition() {
+    const toc = $('#toc');
+    const heroContainer = $('.hero-container');
+    const hasHero = heroContainer.length > 0;
+    const scrollTop = $(window).scrollTop();
+    const heroHeight = hasHero && heroContainer.offset().top + heroContainer.outerHeight();
+
+    if (hasHero) {
+        // With hero image: smoothly transition between positions
+        if (scrollTop < heroHeight - minFromTop) {
+            toc.css('top', `${heroHeight - scrollTop + bufferFromHero}px`);
+        } else {
+            toc.css('top', `${minFromTop}px`);
+        }
+    } else {
+        // Without hero image: adjust based on scroll position
+        if (scrollTop < bufferFromNav - minFromTop) {
+            toc.css('top', `${bufferFromNav}px`);
+        } else {
+            toc.css('top', `${minFromTop}px`);
+        }
+    }
 }
